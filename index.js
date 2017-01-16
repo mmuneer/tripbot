c4b52d85016f37c0260aada89aafe7de2e7fec1a
mmuneer <muntasir.muneer@sabre.com>
2017-01-16 16:40:43 -0600

    bind port with environment

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ app.get('/', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(msg));
 })
-
-app.listen(3000, function () {
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
   console.log('Example app listening on port 3000!')
 })
 
